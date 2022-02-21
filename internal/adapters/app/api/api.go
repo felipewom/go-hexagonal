a61e64cb4e78735bfda6e616a1ef351815b2dd0c
Felipe Moua <felipewom@gmail.com>
2022-02-21 11:26:36 -0300

    Initial commit
    This is an initial commit to start project.

--- internal/adapters/app/api/api.go
+++
@@ -1,9 +0,0 @@
-package api
-
-import (
-	"github.com/felipewom/go-hexagonal/internal/ports"
-)
-
-type Adapter struct {
-	arith ports.ArithmeticPort
-}
--- internal/ports/core.go
+++
@@ -1,8 +0,0 @@
-package ports
-
-type ArithmeticPort interface {
-	Addition(a int32, b int32) (int32, error)
-	Subtraction(a int32, b int32) (int32, error)
-	Multiplication(a int32, b int32) (int32, error)
-	Division(a int32, b int32) (int32, error)
-}
